6aa092d9b307b352d85f9afe1464beffbaa075e3
24331a05v6-create <24331a05v6@mvgrce.edu.in>
2026-06-22 15:38:08 +0530

    sum.py

---
+++ demo.py
@@ -0,0 +1,9 @@
+import sum
+import sum
+import sum
+import sum
+import sum
+
+
+a = sum(2,3)
+print(a)
--- div.py
+++ div.py
@@ -1,11 +1,6 @@
-def divide_numbers(a, b):
-    if b != 0:
-        return a / b
-    else:
-        return "Error: Division by zero is not allowed"
+def sum(a, b):
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
 
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
-
-result = divide_numbers(num1, num2)
-print("Result:", result)
+    result = sum(num1, num2)
+    return result
