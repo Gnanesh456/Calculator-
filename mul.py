6849e5e6e46b1dc59c16f179b072e2313b4dc041
Mohit Naidu <paddanamohitnaidu@gmail.com>
2026-06-22 15:53:03 +0530

    mul

---
+++ mul.py
@@ -0,0 +1,8 @@
+def multiplication():
+    A = float(input("Enter first number: "))
+    B = float(input("Enter second number: "))
+    return A * B
+
+# Call the function and print the result
+print(multiplication())
+
