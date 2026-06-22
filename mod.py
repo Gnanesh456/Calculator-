407e9a9fef02324401c6eed9b7a8f1cb2cecee1e
Gnanesh456 <24331a05u1@mvgrce.edu.in>
2026-06-22 15:52:19 +0530

    Added Modulus mod.py

--- mod.py
+++ mod.py
@@ -1,7 +1,4 @@
 def modulus(a,b):
-    return a%b
-
-n1=float(input("Enter First Number:"))
-n2=float(input("Enter Second Number:"))
-result = modulus(n1,n2)
-print("Modulus=",result)
+    n1=float(input("Enter First Number:"))
+    n2=float(input("Enter Second Number:"))
+    return n1%n2
